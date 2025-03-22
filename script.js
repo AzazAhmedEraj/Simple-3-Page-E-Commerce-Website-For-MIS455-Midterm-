c6a3f2227e15990e70dba7ac885a51a7ea4426ce
AzazAhmedEraj <azaz.a.eraj@gmail.com>
2025-03-22 15:29:03 +0600

    Updated JavaScript Code

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ function showOffer() {
     let button = document.getElementById("offerButton");
     button.textContent = "Congratulations! You unlocked a 10% discount. Use code SHOP10 at checkout.";
     button.disabled = true;
- }
+}
 
- function buyNow() {
+function buyNow() {
     alert("Thanks for shopping with Shop455!");
- }
+}
+
+function showEmail() {
+    alert("Our email: support@shop455.com");
+}
+ 
+ 
+function showPhone() {
+    alert("Call us at: +88 0123456789");
+}
+ 
+  
+function openMap() {
+    window.open("https://maps.app.goo.gl/9kLbeYPADydn6Srm6", "_blank");
+}
  
