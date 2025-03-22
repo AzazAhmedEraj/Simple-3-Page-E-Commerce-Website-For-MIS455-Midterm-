241d1678a449be3ee2038d08383a893f565d8b82
AzazAhmedEraj <azaz.a.eraj@gmail.com>
2025-03-22 13:58:44 +0600

    Updated Button and its function in a JS script

---
+++ script.js
@@ -0,0 +1,6 @@
+function showOffer() {
+    let button = document.getElementById("offerButton");
+    button.textContent = "Congratulations! You unlocked a 10% discount. Use code SHOP10 at checkout.";
+    button.disabled = true;
+ }
+ 
